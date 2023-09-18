fbdf30b043edf8def751959e2c666e0f945a98d1
Shaun-Allan <shaunallan0605@gmail.com>
2023-09-18 13:18:55 +0530

    router rectified

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,19 @@
-
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div>Home</div>
+      <button
+        onClick={() => {
+          navigate("/about");
+        }}
+      >
+        About
+      </button>
+    </>
+  );
+};
 
-export default Home
+export default Home;
