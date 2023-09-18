e02e4422de6907e42f018c595536976c775b4b41
Shaun-Allan <shaunallan0605@gmail.com>
2023-09-18 13:03:10 +0530

    router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import NotFound from "./pages/NotFound/NotFound";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
 function App() {
 
   return (
-    <>
-        Hello
-    </>
-  )
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
-export default App
+export default App;
